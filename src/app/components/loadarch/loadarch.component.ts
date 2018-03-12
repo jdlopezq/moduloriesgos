8531cdf95f22bf083db5be72adc1582fb50cef59
Juan Lopez <juanda520@gmail.com>
2018-03-12 01:06:35 -0500

    cambuos a load

--- src/app/components/loadarch/loadarch.component.ts
+++ src/app/components/loadarch/loadarch.component.ts
@@ -15,65 +15,82 @@ export class LoadarchComponent implements OnInit {
 overDrop:boolean=false;  
 archivos: FileItem[]=[]
 loading: boolean = false;
-constructor(private dataSend: PhpService, private utility:UtilityService, private router: Router, private fb: FormBuilder) { this.createForm();}
-@ViewChild('fileInput') fileInput: ElementRef;
+constructor(private dataSend: PhpService, private utility:UtilityService, private router: Router, private fb: FormBuilder) { 
+  // this.createForm();
+
+}
+// @ViewChild('fileInput') fileInput: ElementRef;
   ngOnInit() {
   }
 
-  sendData(){
 
-    console.log(this.archivos)
-this.dataSend.loadFile(this.archivos, "cargar.php").subscribe(res=>{
-  res;
-  console.log(res)
-})
-
-  }
-
-  pruebaEle(event){
-console.log(event)
-  }
-
-
-  onFileChange(event) {
-    let reader = new FileReader();
-    if(event.target.files && event.target.files.length > 0) {
-      let file = event.target.files[0];
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        this.form.get('avatar').setValue({
-          filename: file.name,
-          filetype: file.type,
-          value: reader.result.split(',')[1]
-        })
-      };
+  uploadFile(event){
+    let elem = event.target
+    if (elem.files.length>0) { 
+      console.log(elem.files[0])
+      let formData =new FormData();
+      formData.append('file', elem.files[0])
+      console.log(formData)
+     
     }
+    
   }
+
+
+//   sendData(){
+
+//     console.log(this.archivos)
+// this.dataSend.loadFile(this.archivos, "cargar.php").subscribe(res=>{
+//   res;
+//   console.log(res)
+// })
+
+  // }
+
+//   pruebaEle(event){
+// console.log(event)
+//   }
+
+
+  // onFileChange(event) {
+  //   let reader = new FileReader();
+  //   if(event.target.files && event.target.files.length > 0) {
+  //     let file = event.target.files[0];
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => {
+  //       this.form.get('avatar').setValue({
+  //         filename: file.name,
+  //         filetype: file.type,
+  //         value: reader.result.split(',')[1]
+  //       })
+  //     };
+  //   }
+  // }
 
 
   
 
-  onSubmit() {
-    const formModel = this.form.value;
-    this.loading = true;
-    // In a real-world app you'd have a http request / service call here like
-    // this.http.post('apiUrl', formModel)
+  // onSubmit() {
+  //   const formModel = this.form.value;
+  //   this.loading = true;
+  //   // In a real-world app you'd have a http request / service call here like
+  //   // this.http.post('apiUrl', formModel)
 
-    this.dataSend.loadFile(formModel, "cargar.php").subscribe(res=>{
-    console.log(res)
-    })
-    setTimeout(() => {
-      console.log(formModel);
-      alert('done!');
-      this.loading = false;
-    }, 1000);
-  }
-  createForm() {
-    this.form = this.fb.group({
-      name: ['', Validators.required],
-      avatar: null
-    });
-  }
+  //   this.dataSend.loadFile(formModel, "cargar.php").subscribe(res=>{
+  //   console.log(res)
+  //   })
+  //   setTimeout(() => {
+  //     console.log(formModel);
+  //     alert('done!');
+  //     this.loading = false;
+  //   }, 1000);
+  // }
+  // createForm() {
+  //   this.form = this.fb.group({
+  //     name: ['', Validators.required],
+  //     avatar: null
+  //   });
+  // }
 
 
 
